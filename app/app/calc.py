59bebf07ad7f0b41f909f3845e1761b61430e1c1
mrrobotoe <aldo.martinez@my.utsa.edu>
2023-12-11 19:37:48 -0600

    fixed lint errors

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,9 +1,11 @@
 """Calculator functions"""
 
+
 def add(x, y):
-  """add x and y and return result"""
-  return x + y
+    """add x and y and return result"""
+    return x + y
+
 
 def subtract(x, y):
-  """subtract y from x"""
-  return x - y
+    """subtract y from x"""
+    return x - y
--- app/app/tests.py
+++ app/app/tests.py
@@ -4,17 +4,18 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
-  """Test the calc module"""
-  
-  def test_add_numbers(self):
-    """Test adding numbers together"""
-    res = calc.add(5, 6)
+    """Test the calc module"""
 
-    self.assertEqual(res, 11)
+    def test_add_numbers(self):
+        """Test adding numbers together"""
+        res = calc.add(5, 6)
 
-  def test_subtract_numbers(self):
-    """Test subtracting numbers"""
-    res = calc.subtract(7, 5)
+        self.assertEqual(res, 11)
 
-    self.assertEqual(res, 2)
+    def test_subtract_numbers(self):
+        """Test subtracting numbers"""
+        res = calc.subtract(7, 5)
+
+        self.assertEqual(res, 2)
