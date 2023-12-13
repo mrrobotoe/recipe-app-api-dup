4d10e4f3cbb9f2f9754b933ed7f5eaf5b292919d
mrrobotoe <aldo.martinez@my.utsa.edu>
2023-12-13 11:44:26 -0600

    added Recipe model and 'makemigrations' ran

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,9 +1,14 @@
 """Tests for models."""
+import datetime
+
+from decimal import Decimal
 
 from django.test import TestCase
 from django.contrib.auth import (
     get_user_model,
 )  # provided by django for default auth model
+
+from core import models
 
 
 class ModelTests(TestCase):
@@ -31,9 +36,7 @@ class ModelTests(TestCase):
         ]
 
         for email, expected in sample_emails:
-            user = get_user_model().objects.create_user(
-                email, "passwordSample123"
-              )
+            user = get_user_model().objects.create_user(email, "passwordSample123")
             self.assertEqual(user.email, expected)
 
     def test_new_user_without_email_raises_error(self):
@@ -43,9 +46,24 @@ class ModelTests(TestCase):
 
     def test_create_superuser(self):
         """Test creating a super user"""
-        user = get_user_model().objects.create_superuser(
-            "test@example.com", "test123"
-          )
+        user = get_user_model().objects.create_superuser("test@example.com", "test123")
         # field provided by the permissions mixin
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """Test creating a recipe is successful."""
+        user = get_user_model().objects.create_user(
+            "test@example.com",
+            "testpass123",
+        )
+
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title="Sample recipe name",
+            time_minutes=5,
+            price=Decimal("5.50"),
+            description="Sample recipe description",
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
--- app/user/views.py
+++ app/user/views.py
@@ -2,11 +2,42 @@
 Views for the User API.
 """
 
-from rest_framework import generics
-from user.serializers import UserSerializer
+from rest_framework import generics, authentication, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+from user.serializers import (
+    UserSerializer,
+    AuthTokenSerializer,
+)
 
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system."""
 
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user."""
+
+    serializer_class = AuthTokenSerializer
+    # gui view on web
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authentication user."""
+
+    serializer_class = UserSerializer
+    # how do you know if the user is the user
+    authentication_classes = [authentication.TokenAuthentication]
+    # we know who you are what is it that they are allowed to do
+    # must be authenticated to use this api
+    permission_classes = [permissions.IsAuthenticated]
+
+    # override  retrieving the user the request.
+    # gets assigned the user to the request object
+    def get_object(self):
+        """Retrieve and return the authenticated user."""
+        return self.request.user
