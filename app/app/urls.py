1a44f9f780543a0e853a95387660230bbc0e83f6
mrrobotoe <aldo.martinez@my.utsa.edu>
2024-05-31 09:34:31 -0500

    added recipe model and testing

--- app/app/urls.py
+++ app/app/urls.py
@@ -24,6 +24,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/user/", include("user.urls")),
+    path("api/recipe/", include("recipe.urls")),
     path("api/schema", SpectacularAPIView.as_view(), name="api-schema"),
     path(
         "api/docs/",
